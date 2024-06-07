3564f44ee0add6596083cda85b9aef0376c2235c
Nguyen Huu Minh <nghuuminh03@gmail.com>
2024-06-07 22:00:21 +0700

    piece self-value and position-based value

--- ChessBot/ChessBot/Core/Evaluation/Evaluator.cs
+++ ChessBot/ChessBot/Core/Evaluation/Evaluator.cs
@@ -4,8 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ChessBot.Core.Board;
+
 namespace ChessBot.Core.Evaluation;
 
-public class Evaluator
-{
+public class Evaluator {
+    public const int PawnValue = 100;
+    public const int KnightValue = 300;
+    public const int BishopValue = 320;
+    public const int RookValue = 500;
+    public const int QueenValue = 900;
+
+    private Board.Board _board;
+    public int Evaluate(Board.Board board) {
+        _board = board;
+
+        return 0;
+    }
+
 }
